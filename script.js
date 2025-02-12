4a67746286aa2ae9b2bdd6f0d65359f0fede3bbe
juliasa13 <juliasbaiao@gmail.com>
2025-02-12 16:53:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const products = [
     { id: 1, name: "Faqueiro", description: "Facas de inox.", price: 380, image: "https://i.postimg.cc/R0BqWcCG/nanjing-10297-1280.jpg", link: "https://mpago.la/28JZ3PA" },
     { id: 2, name: "Mesa de Jantar", description: "Para sentar com a família.", price: 960, image: "https://i.postimg.cc/PrvqK6P2/living-room-8215136-1280.jpg", link: "https://mpago.la/1RoDpT7" },
-    { id: 3, name: "Sofá 2 Lugares", description: "Confortável e estiloso.", price: 5200, image: "https://i.postimg.cc/GhtDRJ9y/real-estate-6893060-1280-1.jpg", link: "https://mpago.la/2nnsbVJ" },
+    { id: 3, name: "Mesas de Cabeceira", description: "Conjunto de 2 mesas funcionais.", price: 1850, image: "https://i.postimg.cc/Twcbdtrr/in-the-morning-4652076-1280.jpg", link: "https://mpago.la/1YT2wKN" },
     { id: 4, name: "Máquina de Lavar", description: "Capacidade de 11kg.", price: 2500, image: "https://i.postimg.cc/fRGJmwYR/washing-machine-1994661-1280.png", link: "https://mpago.la/1jM1jRz" },
     { id: 5, name: "Jogo de Panelas", description: "Conjunto de 5 peças antiaderentes.", price: 300, image: "https://i.postimg.cc/SQTXg2VN/cook-750142-1280.jpg", link: "https://mpago.la/15gTR9w" },
     { id: 6, name: "Closet", description: "Estrutura em madeira MDF.", price: 1500, image: "https://i.postimg.cc/sgBfDtQ9/closet-4696557-1280.jpg", link: "https://mpago.la/27HKrJA" },
@@ -16,7 +16,7 @@ const products = [
     { id: 12, name: "Cadeiras", description: "Conjunto de 2 cadeiras.", price: 900, image: "https://i.postimg.cc/wM7jC2Fx/breakfast-7045227-1280.jpg", link: "https://mpago.la/15sD4gZ" },
     { id: 13, name: "Mesa Lateral", description: "Compacta e prática para sua sala.", price: 620, image: "https://i.postimg.cc/vHKVZg6S/living-room-table-4037281-1280.jpg", link: "https://mpago.la/2zb6VSX" },
     { id: 14, name: "Tapete", description: "Grande e aconchegante.", price: 1900, image: "https://i.postimg.cc/RZcZxN1t/furniture-998265-1280.jpg", link: "https://mpago.la/18s6Zq3" },
-    { id: 15, name: "Mesas de Cabeceira", description: "Conjunto de 2 mesas funcionais.", price: 1850, image: "https://i.postimg.cc/Twcbdtrr/in-the-morning-4652076-1280.jpg", link: "https://mpago.la/1YT2wKN" },
+    { id: 15, name: "Sofá 2 Lugares", description: "Confortável e estiloso.", price: 5200, image: "https://i.postimg.cc/GhtDRJ9y/real-estate-6893060-1280-1.jpg", link: "https://mpago.la/2nnsbVJ" },
 ];
 
 // Renderizar os produtos na página
